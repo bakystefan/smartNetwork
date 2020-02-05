ec159c2d6307b3e0031a30db62a39476bb67bfca
lookagit <simjanovic.luka@gmail.com>
2020-02-05 02:46:56 +0100

    UI for login screen

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import {
   View,
-  Text,
-  StyleSheet
+  StyleSheet,
+  KeyboardAvoidingView,
+  ScrollView,
+  ImageBackground,
+  Platform,
 } from 'react-native';
-// import { bindActionCreators } from  'redux';
-import { connect } from 'react-redux';
+import LoginScreenHeader from '../../components/loginScreenHeader';
+import LoginScreenMain from '../../components/loginScreenMain';
 
-const LoginScreen = ({ auth }) => (
-  <View style={styles.container}>
-    <Text>LoginScreen</Text>
-  </View>
-)
+const LoginScreen = ({ auth }) => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.select({ ios: 'padding' })}
+    >
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardDismissMode="interactive"
+        keyboardShouldPersistTaps="handled"
+      >
+        <ImageBackground 
+          source={require('../../assets/images/loginBackgroundCircle.png')} 
+          style={styles.backgroundStyle}
+        >
+          <View style={{ flex: 1 }}>
+            <LoginScreenHeader />
+            <LoginScreenMain
+              password={password}
+              email={email}
+              setEmail={setEmail}
+              setPassword={setPassword}
+              loginFunc={() => {}}
+            />
+          </View>
+        </ImageBackground>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#5F72FF'
+  },
+  backgroundStyle: { 
+    flex: 1,
+    width: '100%'
   }
 });
 
 const mapStateToProps = state => ({ auth: state.auth });
-
 
 export default connect(mapStateToProps)(LoginScreen);
