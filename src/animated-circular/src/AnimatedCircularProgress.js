dee73723d480e177f40f104497110d38e12ee094
bakydalabu <stefanbakmaz@gmail.com>
2020-02-10 15:09:36 +0100

    added graph circular component

---
+++ src/animated-circular/src/AnimatedCircularProgress.js
@@ -0,0 +1,278 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Animated, Easing } from 'react-native';
+import CircularProgress from './CircularProgress';
+const AnimatedProgress = Animated.createAnimatedComponent(CircularProgress);
+
+export default class AnimatedCircularProgress extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fillAnimation: new Animated.Value(props.prefill),
+      secondAnimation: new Animated.Value(0),
+      thirdAnimation: new Animated.Value(0),
+      secondCircularFillAnimation: new Animated.Value(0),
+      secondCircularSecondAnimation: new Animated.Value(0),
+      secondCircularThirdAnimation: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    this.animate();
+    this.animateSecond();
+    this.animateThird();
+    this.secondCircularAnimate();
+    this.secondCircularAnimateSecond();
+    this.secondCircularAnimateThird();
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log("BLA",prevProps.secondCircularThirdFill,  this.props.secondCircularThirdFill)
+    if (prevProps.fill !== this.props.fill) {
+      this.animate();
+    }
+    if (prevProps.secondFill !== this.props.secondFill) {
+      this.animateSecond();
+    }
+    if (prevProps.thirdFill !== this.props.thirdFill) {
+      this.animateThird();
+    }
+    if (prevProps.secondCircularFill !== this.props.secondCircularFill) {
+      this.secondCircularAnimate();
+    }
+    if (prevProps.secondCircularSecondFill !== this.props.secondCircularSecondFill) {
+      this.secondCircularAnimateSecond();
+    }
+    if (prevProps.secondCircularThirdFill !== this.props.secondCircularThirdFill) {
+      
+      this.secondCircularAnimateThird()
+    }
+  }
+
+  reAnimate(prefill, toVal, dur, ease) {
+    this.setState(
+      {
+        fillAnimation: new Animated.Value(prefill),
+        secondAnimation: new Animated.Value(0),
+        thirdAnimation: new Animated.Value(0),
+        secondCircularFillAnimation: new Animated.Value(0),
+        secondCircularSecondAnimation: new Animated.Value(0),
+        secondCircularThirdAnimation: new Animated.Value(0),
+      },
+      () => {
+        this.animate(toVal, dur, ease);
+        this.animateSecond(toVal, dur, ease);
+        this.animateThird(toVal, dur, ease);
+        this.secondCircularAnimate(toVal, dur, ease);
+        this.secondCircularAnimateSecond(toVal, dur, ease);
+        this.secondCircularAnimateThird(toVal, dur, ease);
+      }
+    );
+  }
+
+  animate(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.fill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.fillAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  animateSecond(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.secondFill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.secondAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  animateThird(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.thirdFill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.thirdAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  animateColor() {
+    if (!this.props.tintColorSecondary) {
+      return this.props.tintColor
+    }
+
+    const tintAnimation = this.state.fillAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.tintColor, this.props.tintColorSecondary]
+    })
+
+    return tintAnimation
+  }
+
+  animateSecondColor() {
+    const tintAnimation = this.state.secondAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.secondColor, this.props.secondColor]
+    })
+
+    return tintAnimation
+  }
+
+  animateThirdColor() {
+    const tintAnimation = this.state.thirdAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.thirdColor, this.props.thirdColor]
+    })
+
+    return tintAnimation
+  }
+  // **** ANIMATION FOR SECOND CIRCULAR PROGRESS! ****
+
+  secondCircularAnimate(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.secondCircularFill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.secondCircularFillAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  secondCircularAnimateSecond(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.secondCircularSecondFill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.secondCircularSecondAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  secondCircularAnimateThird(toVal, dur, ease) {
+    const toValue = toVal >= 0 ? toVal : this.props.secondCircularThirdFill;
+    const duration = dur || this.props.duration;
+    const easing = ease || this.props.easing;
+    const useNativeDriver = this.props.useNativeDriver;
+    
+    const anim = Animated.timing(this.state.secondCircularThirdAnimation, {
+      useNativeDriver,
+      toValue,
+      easing,
+      duration,
+    });
+    anim.start(this.props.onAnimationComplete);
+
+    return anim;
+  }
+
+  secondCircularAnimateColor() {
+    const tintAnimation = this.state.secondCircularFillAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.secondCircularTintColor, this.props.secondCircularTintColor]
+    })
+
+    return tintAnimation
+  }
+
+  secondCircularAnimateSecondColor() {
+    const tintAnimation = this.state.secondCircularSecondAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.secondCircularSecondColor, this.props.secondCircularSecondColor]
+    })
+
+    return tintAnimation
+  }
+
+  secondCircularAnimateThirdColor() {
+    const tintAnimation = this.state.secondCircularThirdAnimation.interpolate({
+      inputRange: [0, 100],
+      outputRange: [this.props.secondCircularThirdColor, this.props.secondCircularThirdColor]
+    })
+
+    return tintAnimation
+  }
+
+  render() {
+    const { fill, prefill, ...other } = this.props;
+    const {
+      fillAnimation,
+      secondAnimation,
+      thirdAnimation,
+      secondCircularFillAnimation,
+      secondCircularSecondAnimation,
+      secondCircularThirdAnimation
+    } = this.state;
+    return (
+      <AnimatedProgress
+        {...other}
+        fill={fillAnimation}
+        tintColor={this.animateColor()}
+        secondFill={secondAnimation}
+        secondColor={this.animateSecondColor()}
+        thirdFill={thirdAnimation}
+        thirdColor={this.animateThirdColor()}
+        secondCircularFill={secondCircularFillAnimation}
+        secondCircularTintColor={this.secondCircularAnimateColor()}
+        secondCircularSecondFill={secondCircularSecondAnimation}
+        secondCircularSecondColor={this.secondCircularAnimateSecondColor()}
+        secondCircularThirdFill={secondCircularThirdAnimation}
+        secondCircularThirdColor={this.secondCircularAnimateThirdColor()}
+      />
+    );
+  }
+}
+
+AnimatedCircularProgress.propTypes = {
+  ...CircularProgress.propTypes,
+  prefill: PropTypes.number,
+  duration: PropTypes.number,
+  easing: PropTypes.func,
+  onAnimationComplete: PropTypes.func,
+  useNativeDriver: PropTypes.bool,
+};
+
+AnimatedCircularProgress.defaultProps = {
+  duration: 500,
+  easing: Easing.out(Easing.ease),
+  prefill: 0,
+  useNativeDriver: false,
+};
---
+++ src/screens/DashboardScreen/index.tsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+// import { bindActionCreators } from  'redux';
+import { connect } from 'react-redux';
+import { AnimatedCircularProgress } from '../../animated-circular';
+import { LATO_BOLD } from '../../assets/fonts';
+
+const DashboardScreen = ({ auth }) => {
+  return (
+    <View style={styles.container}>
+      <AnimatedCircularProgress
+        size={220}
+        secondCicularSize={264}
+        arcSweepAngle={270}
+        width={20}
+        rotation={0}
+        fill={100}
+        tintColor="#FFFFFF"
+        lineCap='round'
+        secondFill={75}
+        secondColor="#D9D9D9"
+        thirdFill={50}
+        thirdColor="#5F72FF"
+        secondCircularFill={100}
+        secondCircularTintColor="#FFFFFF"
+        secondCircularSecondFill={85}
+        secondCircularSecondColor="#D9D9D9"
+        secondCircularThirdFill={70}
+        secondCircularThirdColor="#6EE294"
+        duration={1200}
+        showArrow
+      >
+        {fill => (
+          <View style={styles.innerContainer}>
+            <View style={styles.currentSpeedBox}>
+              <Text style={styles.currentSpeedText}>CURRENT SPEED</Text>
+            </View>
+            <View style={styles.rowBox}>
+              <View style={styles.downColumn}>
+                <Text style={styles.currentSpeedDlValue}>102.2</Text>
+                <View style={styles.ispCalibrateTitleBox}>
+                  <Text style={styles.ispCalibrateTitle}>ISP SPEED</Text>
+                </View>
+                <View style={styles.rowIspCalibrate}>
+                  <Text style={styles.greenValue}>10.3</Text>
+                  <Text style={styles.blueValue}>2.4</Text>
+                </View>
+                <View style={styles.roundWhite} />
+              </View>
+              <View style={styles.upColumn}>
+                <Text style={styles.currentSpeedUpValue}>33.2</Text>
+                <View style={styles.ispCalibrateTitleBox}>
+                  <Text style={styles.ispCalibrateTitle}>CALIBRATED</Text>
+                </View>
+                <View style={[styles.rowIspCalibrate, { justifyContent: 'space-evenly' }]}>
+                  <Text style={styles.greenValue}>15.0</Text>
+                  <Text style={styles.blueValue}>1.4</Text>
+                </View>
+                <View style={styles.roundGray} />
+              </View>
+            </View>
+            <View style={styles.unitMbitBox}>
+              <Text style={styles.unitMbitText}>Unit: Mbit</Text>
+            </View>
+          </View>
+        )}
+      </AnimatedCircularProgress>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(245,245,245)'
+  },
+  innerContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: 140,
+    flex: 1,
+  },
+  currentSpeedBox: {
+    paddingTop: 30
+  },
+  downColumn: {
+    flexDirection: 'column',
+  },
+  upColumn: {
+    flexDirection: 'column',
+  },
+  currentSpeedText: {
+    color: '#929292',
+    fontSize: 12,
+    letterSpacing: 0.08,
+    lineHeight: 13,
+    textAlign: 'center',
+    fontFamily: LATO_BOLD
+  },
+  rowBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  currentSpeedDlValue: {
+    color: '#4EA169',
+    textAlign: 'center',
+    fontSize: 19,
+    fontFamily: LATO_BOLD
+  },
+  currentSpeedUpValue: {
+    color: '#424FB2',
+    textAlign: 'center',
+    fontSize: 19,
+    fontFamily: LATO_BOLD
+  },
+  ispCalibrateTitleBox: {
+    marginTop: 20,
+    marginBottom: 5
+  },
+  ispCalibrateTitle: {
+    color: '#929292',
+    fontSize: 11,
+    lineHeight: 11,
+    letterSpacing: 0.07,
+    fontFamily: LATO_BOLD,
+    textAlign: 'left'
+  },
+  rowIspCalibrate: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  greenValue: {
+    color: '#4EA169',
+    fontSize: 12,
+    fontFamily: LATO_BOLD,
+    letterSpacing: 0.08
+  },
+  blueValue: {
+    color: '#424FB2',
+    fontSize: 12,
+    fontFamily: LATO_BOLD,
+    letterSpacing: 0.08
+  },
+  roundWhite: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: '#FFFFFF',
+    borderColor: '#D9D9D9',
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 5
+  },
+  roundGray: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: '#D9D9D9',
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 5
+  },
+  unitMbitBox: {
+    paddingVertical: 10,
+    alignItems: 'center'
+  },
+  unitMbitText: {
+    color: '#929292',
+    fontSize: 12,
+    fontFamily: LATO_BOLD
+  }
+});
+
+const mapStateToProps = state => ({ auth: state.auth });
+
+
+export default connect(mapStateToProps)(DashboardScreen);
